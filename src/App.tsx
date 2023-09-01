a10b53bcf6a4d2679fe7e0cd90286a3e9b438f8d
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-09-01 16:00:58 +0800

    Add header with nav buttons and brand

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header/Header"
+// import './outline.css';
+
 function App() {
 
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+
+      <h1 id="logo">
+        <div>Lee</div>
+        <div>Ryan</div>
+      </h1>
+
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Experience</li>
+        <li>Education</li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
