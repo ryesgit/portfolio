b7bcbaca30b01320bb15eed2c51cd98cb7421ef4
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-09-02 09:44:45 +0800

    Make Hero a component of Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,8 @@
 
 import Header from "../../components/Header/Header"
 import Dashes from "../../components/ui/Dashes"
-import Logo from "../../components/ui/Logo/Logo"
 import "./Home.css"
-import { motion } from "framer-motion"
+import Hero from "./components/Hero"
 
 function Home() {
 
@@ -15,38 +14,9 @@ function Home() {
 
         <Dashes color="red" />
 
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ rotate: 360, scale: 1 }}
-          transition={{
-            type: "spring",
-            stiffness: 260,
-            damping: 20
-          }}>
+        <Hero id="hero" />
 
-          <Logo style={{
-            width: "10rem",
-            maxHeight: "10rem",
-            marginTop: "1rem"
-          }}/>
-
-        </motion.div>
-
-          <article>
-
-            <h1>Lee Ryan Soliman</h1>
-
-            <h2>Student, Developer, Problem Solver</h2>
-
-            <p style={{ lineHeight: "1.5rem" }}>
-              incoming Computer Engineering sophomore; <br />
-              helping clients with tech problems through freelance work, <br />
-              while building fun toy projects on the side
-            </p>
-
-          </article>
-
-          <Dashes color="red" />
+        <Dashes color="red" />
 
       </main>
     </>
