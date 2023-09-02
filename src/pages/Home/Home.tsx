2d5fcbc7a6a12c4aac156186265d26bf4e40ad2c
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-09-02 09:07:00 +0800

    Add motion effect to hero logo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import Header from "../../components/Header/Header"
 import Dashes from "../../components/ui/Dashes"
 import Logo from "../../components/ui/Logo/Logo"
 import "./Home.css"
+import { motion } from "framer-motion"
 
 function Home() {
 
@@ -11,12 +12,26 @@ function Home() {
     <>
       <Header />
       <main>
-          <Dashes color="red" />
+
+        <Dashes color="red" />
+
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ rotate: 360, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20
+          }}>
+
           <Logo style={{
             width: "10rem",
             maxHeight: "10rem",
             marginTop: "1rem"
           }}/>
+
+        </motion.div>
+
       </main>
     </>
   )
