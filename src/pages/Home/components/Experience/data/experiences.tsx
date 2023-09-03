05b8edcd3e5f6d4afa59ecf7ad3507f58c9567e4
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-09-03 17:10:05 +0800

    Add experiences section

---
+++ src/pages/Home/components/Experience/data/experiences.tsx
@@ -0,0 +1,28 @@
+interface experience {
+    jobTitle: string,
+    company: string,
+    startDate: string,
+    endDate: string,
+    description: string[]
+}
+
+const experiences: experience[] = [
+    {
+        jobTitle: "Backend Developer",
+        company: "Saphlink",
+        startDate: "May 2023",
+        endDate: "August 2023",
+        description: [
+                    "First time working with a team.",
+                    "Built the backend of a calendar application that\
+                     integrated Google Calendar and Microsoft Outlook\
+                     Calendar using Node.js, Express.js, and MongoDB.",
+                    
+                    "Wrote an automatic email-sending script that takes\
+                    a list of emails and sends each a message using\
+                    nodemailer, a Node.js module."
+                    ]
+    }
+];
+
+export default experiences;
--- src/pages/Home/components/Experience/index.tsx
+++ src/pages/Home/components/Experience/index.tsx
@@ -1,7 +1,36 @@
+import experiences from "./data/experiences"
+import "./Experience.css";
+
 const Experience = () => {
   return (
     <section id="experience">
         <h2>Experience</h2>
+        {    
+            experiences.map(experience => {
+                return (
+                    <div className="experience" key={experience.company}>
+                        <h2>{experience.company}</h2>
+                        <h3>{experience.jobTitle}</h3>
+
+                        <p>{experience.startDate} - {experience.endDate}</p>
+
+                        <ul>
+                        {
+                            experience.description.map((description, index) => {
+                                return (
+                                    <li key={index}>
+                                        {description}
+                                    </li>
+                                )
+                            })
+                        }
+
+                        </ul>
+                    </div>
+                )
+            })
+        }
+
     </section>
   )
 }
