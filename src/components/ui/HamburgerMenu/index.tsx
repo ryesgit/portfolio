ab898dd7ae0ab69eedd58cbcc3ed67f4111accfe
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-09-04 09:23:03 +0800

    Transform hamburger to x button

--- src/components/ui/HamburgerMenu/index.tsx
+++ src/components/ui/HamburgerMenu/index.tsx
@@ -1,13 +1,16 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 import "./HamburgerMenu.css";
 
 const HamburgerMenu = ({ id, style, barsStyle }: { id: string, style? : CSSProperties, barsStyle? : CSSProperties}) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
     <aside id={id} style={ style }>
-        <div id="bars" style={ barsStyle }>
-            <div className="bars"></div>
-            <div className="bars"></div>
-            <div className="bars"></div>
+        <div className={ `bars ${ isOpen ? 'x' : '' }` } style={ barsStyle } onClick={() => setIsOpen( !isOpen )}>
+            <div id="bars"></div>
+            <div id="bars"></div>
+            <div id="bars"></div>
         </div>
     </aside>
   )
