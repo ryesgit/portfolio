5c11fcd7e4003bb1b8e7618b44d488bc30438309
codenamerey <100455152+codenamerey@users.noreply.github.com>
2024-01-11 21:49:32 +0800

    Add OM.Farm to experience

--- src/pages/Home/components/Experience/data/experiences.tsx
+++ src/pages/Home/components/Experience/data/experiences.tsx
@@ -8,6 +8,13 @@ interface experience {
 
 const experiences: experience[] = [
     {
+        jobTitle: "Software Engineering Intern",
+        company: "OM.FARM",
+        startDate: "January 2024",
+        endDate: "Present",
+        description: []
+    },
+    {
         company: "Google Developer Student Clubs",
         jobTitle: "Software Development Cadet",
         startDate: "October 2023",
@@ -20,16 +27,16 @@ const experiences: experience[] = [
         startDate: "May 2023",
         endDate: "August 2023",
         description: [
-                    "First time working with a team.",
-                    "Built the backend of a calendar application that\
+            "First time working with a team.",
+            "Built the backend of a calendar application that\
                      integrated Google Calendar and Microsoft Outlook\
                      Calendar using Express.js and MongoDB.",
-                    
-                    "Wrote an automatic email-sending script that takes\
+
+            "Wrote an automatic email-sending script that takes\
                     a list of emails and sends each a message using\
                     nodemailer, a Node.js module."
-                    ]
-    },
+        ]
+    }
 ];
 
 export default experiences;
