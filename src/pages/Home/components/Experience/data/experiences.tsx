9cd40eb8e6bb0ec06b140000a4f842d1a707622f
ryesgit <128949576+ryesgit@users.noreply.github.com>
2024-05-01 11:39:24 +0800

    Update experience (OM.Farm)

--- src/pages/Home/components/Experience/data/experiences.tsx
+++ src/pages/Home/components/Experience/data/experiences.tsx
@@ -1,42 +1,46 @@
 interface experience {
-    jobTitle: string,
-    company: string,
-    startDate: string,
-    endDate: string,
-    description: string[]
+	jobTitle: string;
+	company: string;
+	startDate: string;
+	endDate: string;
+	description: string[];
 }
 
 const experiences: experience[] = [
-    {
-        jobTitle: "Software Engineering Intern",
-        company: "OM.FARM",
-        startDate: "January 2024",
-        endDate: "Present",
-        description: []
-    },
-    {
-        company: "Google Developer Student Clubs",
-        jobTitle: "Software Development Subject Matter Expert (SME)",
-        startDate: "October 2023",
-        endDate: "Present",
-        description: []
-    },
-    {
-        jobTitle: "Backend Developer",
-        company: "Saphlink",
-        startDate: "May 2023",
-        endDate: "August 2023",
-        description: [
-            "First time working with a team.",
-            "Built the backend of a calendar application that\
+	{
+		jobTitle: "Software Engineering Intern",
+		company: "OM.FARM",
+		startDate: "January 2024",
+		endDate: "Present",
+		description: [
+			"Working closely with two senior developers and two co-interns to develop frontend and backend services for a livestock leasing platform.",
+			"Using Nest.js for the backend; helped building a GraphQL API",
+			"Learning the CQRS design pattern, best practices for writing clean code; bolstered understanding of the SOLID principles",
+		],
+	},
+	{
+		company: "Google Developer Student Clubs",
+		jobTitle: "Software Development Subject Matter Expert (SME)",
+		startDate: "October 2023",
+		endDate: "Present",
+		description: [],
+	},
+	{
+		jobTitle: "Backend Developer",
+		company: "Saphlink",
+		startDate: "May 2023",
+		endDate: "August 2023",
+		description: [
+			"First time working with a team.",
+			"Built the backend of a calendar application that\
                      integrated Google Calendar and Microsoft Outlook\
                      Calendar using Express.js and MongoDB.",
 
-            "Wrote an automatic email-sending script that takes\
+			"Wrote an automatic email-sending script that takes\
                     a list of emails and sends each a message using\
-                    nodemailer, a Node.js module."
-        ]
-    }
+                    nodemailer, a Node.js module.",
+		],
+	},
 ];
 
 export default experiences;
