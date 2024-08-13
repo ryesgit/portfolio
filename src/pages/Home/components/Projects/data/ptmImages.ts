647a5dc96531745833004f34465d9e197eaa8c38
ryesgit <128949576+ryesgit@users.noreply.github.com>
2024-08-13 18:36:06 +0800

    feat(projects): add pass the message

---
+++ src/pages/Home/components/Projects/data/ptmImages.ts
@@ -0,0 +1,7 @@
+import ptm1 from "../assets/ptm-1.webp";
+import ptm2 from "../assets/ptm-2.webp";
+import ptm3 from "../assets/ptm-3.webp";
+import ptm4 from "../assets/ptm-4.webp";
+import ptm5 from "../assets/ptm-5.webp";
+
+export const ptmImages = [ptm1, ptm2, ptm3, ptm4, ptm5];
