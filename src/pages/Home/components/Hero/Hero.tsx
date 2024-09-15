37c114f89ef385eb810df425300c8da17ab84c6d
Ryan <128949576+ryesgit@users.noreply.github.com>
2024-09-15 23:02:54 +0800

    refactor(projects): update experience, hero text

--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -1,63 +1,54 @@
 import { motion } from "framer-motion";
-import "./Hero.css";
-import links from "./data/links";
 import { useContext } from "react";
 import DarkModeContext from "../../../../contexts/DarkModeProvider";
+import "./Hero.css";
+import links from "./data/links";
 
-const Hero = ({ id } : { id?:string }) => {
-
-    const { dark } = useContext(DarkModeContext)
+const Hero = ({ id }: { id?: string }) => {
+  const { dark } = useContext(DarkModeContext);
 
   return (
-        <section id={id}>
-            <motion.div
-            initial={{ scale: 0 }}
-            animate={{ rotate: 360, scale: 1 }}
-            transition={{
-            type: "spring",
-            stiffness: 260,
-            damping: 20
-            }}>
-            
-            </motion.div>
+    <section id={id}>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ rotate: 360, scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+      ></motion.div>
 
+      <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
+        <h1>Lee Ryan Soliman</h1>
+        <div className="links">
+          {links.map((link, index) => {
+            return (
+              <span
+                id="link"
+                key={index}
+                className={`${!dark ? "lightLink" : ""}`}
+              >
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  {link.icon} <br />
+                  <p>{link.platform}</p>
+                </a>
+              </span>
+            );
+          })}
+        </div>
 
-            <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}>
+        <article>
+          <h2>Student, Developer, Problem Solver</h2>
+          <p>
+            Computer Engineering senior; <br />
+            Software problem solver <br />
+            and a passionate developer.
+          </p>
+        </article>
+      </motion.div>
+    </section>
+  );
+};
 
-                <h1>Lee Ryan Soliman</h1>
-                <div className="links">
-
-                    {
-                        links.map((link, index) => {
-                            return (
-                                <span id="link" key={index} className={`${ !dark ? "lightLink" : "" }`}>
-                                    <a href={link.url} target="_blank" rel="noreferrer">
-                                        {link.icon} <br /> 
-                                            <p>
-                                            {link.platform}
-                                            </p>
-                                    </a>
-                                </span>
-                            )
-                        })
-                    }
-
-                </div>
-
-                <article>
-                <h2>Student, Developer, Problem Solver</h2>
-                <p>
-                    Computer Engineering sophomore; <br />
-                    Helping clients with tech problems <br />
-                    While building fun toy projects on the side
-                </p>
-                </article>
-            </motion.div>
-
-        </section>
-  )
-}
-
-export default Hero
+export default Hero;
