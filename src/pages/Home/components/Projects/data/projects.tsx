b6021c6e84b550eb686e117eb08f0409e2267dc0
Ryan <128949576+ryesgit@users.noreply.github.com>
2024-09-20 08:41:05 +0800

    refactor: update television project live link

--- src/pages/Home/components/Projects/data/projects.tsx
+++ src/pages/Home/components/Projects/data/projects.tsx
@@ -9,89 +9,90 @@ import { ptmImages } from "./ptmImages";
 
 import { ReactNode } from "react";
 import {
-	SiCss3,
-	SiFirebase,
-	SiGraphql,
-	SiJavascript,
-	SiMongodb,
-	SiNextdotjs,
-	SiNodedotjs,
-	SiPostgresql,
-	SiPrisma,
-	SiPython,
-	SiReact,
-	SiTailwindcss,
-	SiTypescript,
+  SiCss3,
+  SiFirebase,
+  SiGraphql,
+  SiJavascript,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiPrisma,
+  SiPython,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
 } from "react-icons/si";
 import Slideshow from "../../../../../components/ui/Slideshow/index";
 
 interface project {
-	name: string;
-	projectImages?: ReactNode;
-	technologiesUsed: ReactNode[];
-	liveLink?: string;
-	githubLink?: string;
+  name: string;
+  projectImages?: ReactNode;
+  technologiesUsed: ReactNode[];
+  liveLink?: string;
+  githubLink?: string;
 }
 
 const projects: project[] = [
-	{
-		name: "Chug Blogs",
-		technologiesUsed: [
-			<SiTypescript />,
-			<SiNodedotjs />,
-			<SiMongodb />,
-			<SiTailwindcss />,
-		],
-		projectImages: <Slideshow images={[cb1, cb2, cb3, cb4, cb5]} />,
-		githubLink: "https://github.com/codenamerey/chug-blogs-client",
-	},
-	{
-		name: "Harry Potter Memory Game",
-		technologiesUsed: [<SiReact />, <SiJavascript />, <SiCss3 />],
-		projectImages: <img src={hpmg1} />,
-		githubLink: "https://github.com/codenamerey/memory-card",
-		liveLink: "https://codenamerey.github.io/memory-card/",
-	},
-	{
-		name: "Covid-19 Contact-Tracing Application",
-		technologiesUsed: [<SiPython />],
-		projectImages: <Slideshow images={[ccta1, ccta2]} />,
-		githubLink: "https://github.com/ryesgit/covid-contract-tracing",
-	},
-	{
-		name: "Television",
-		technologiesUsed: [
-			<SiPython />,
-			<SiReact />,
-			<SiJavascript />,
-			<SiTailwindcss />,
-		],
-		projectImages: <Slideshow images={[tv1, tv2]} />,
-		githubLink: "https://github.com/ryesgit/television",
-		liveLink: "https://television.onrender.com/",
-	},
-	{
-		name: "Set Relation Determinator",
-		technologiesUsed: [<SiReact />, <SiJavascript />, <SiCss3 />],
-		githubLink: "https://github.com/codenamerey/set-relation-determinator-tdd",
-		liveLink: "https://codenamerey.github.io/set-relation-determinator-tdd/",
-		projectImages: <img src={srd1} />,
-	},
-	{
-		name: "Pass The Message",
-		technologiesUsed: [
-			<SiNextdotjs />,
-			<SiGraphql />,
-			<SiTailwindcss />,
-			<SiFirebase />,
-			<SiTypescript />,
-			<SiPrisma />,
-			<SiPostgresql />,
-		],
-		projectImages: <Slideshow images={ptmImages} />,
-		liveLink:
-			"https://drive.google.com/file/d/1IotI6KuTzlSoXA_CtzYX-ArA9JFiA1NA/view?usp=sharing",
-	},
+  {
+    name: "Chug Blogs",
+    technologiesUsed: [
+      <SiTypescript />,
+      <SiNodedotjs />,
+      <SiMongodb />,
+      <SiTailwindcss />,
+    ],
+    projectImages: <Slideshow images={[cb1, cb2, cb3, cb4, cb5]} />,
+    githubLink: "https://github.com/codenamerey/chug-blogs-client",
+  },
+  {
+    name: "Harry Potter Memory Game",
+    technologiesUsed: [<SiReact />, <SiJavascript />, <SiCss3 />],
+    projectImages: <img src={hpmg1} />,
+    githubLink: "https://github.com/codenamerey/memory-card",
+    liveLink: "https://codenamerey.github.io/memory-card/",
+  },
+  {
+    name: "Covid-19 Contact-Tracing Application",
+    technologiesUsed: [<SiPython />],
+    projectImages: <Slideshow images={[ccta1, ccta2]} />,
+    githubLink: "https://github.com/ryesgit/covid-contract-tracing",
+  },
+  {
+    name: "Television",
+    technologiesUsed: [
+      <SiPython />,
+      <SiReact />,
+      <SiJavascript />,
+      <SiTailwindcss />,
+    ],
+    projectImages: <Slideshow images={[tv1, tv2]} />,
+    githubLink: "https://github.com/ryesgit/television",
+    liveLink:
+      "https://drive.google.com/file/d/1Gsu5pv-hXmPWHcitsasSRx1-hywGZ6fG/view?usp=sharing",
+  },
+  {
+    name: "Set Relation Determinator",
+    technologiesUsed: [<SiReact />, <SiJavascript />, <SiCss3 />],
+    githubLink: "https://github.com/codenamerey/set-relation-determinator-tdd",
+    liveLink: "https://codenamerey.github.io/set-relation-determinator-tdd/",
+    projectImages: <img src={srd1} />,
+  },
+  {
+    name: "Pass The Message",
+    technologiesUsed: [
+      <SiNextdotjs />,
+      <SiGraphql />,
+      <SiTailwindcss />,
+      <SiFirebase />,
+      <SiTypescript />,
+      <SiPrisma />,
+      <SiPostgresql />,
+    ],
+    projectImages: <Slideshow images={ptmImages} />,
+    liveLink:
+      "https://drive.google.com/file/d/1IotI6KuTzlSoXA_CtzYX-ArA9JFiA1NA/view?usp=sharing",
+  },
 ];
 
 export default projects;
